4fb9d1234db10b62b2cc14812cb4b961c5c43893
ralp873 <78292822+ralp873@users.noreply.github.com>
2023-06-18 14:29:30 -0600

    Upload index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNavDropdown')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+
+window.addEventListener('resize', () => {
+  if(window.innerWidth>=768) {
+    navLinks.forEach((l) => {
+      l.addEventListener('click', () => { bsCollapse.toggle() })
+  })
+  }
+});
+
