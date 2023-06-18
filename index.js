6af75bf07f1ddedcd50e5ba6c581fcea0948d97f
ralp873 <78292822+ralp873@users.noreply.github.com>
2023-06-18 14:23:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const menuToggle = document.getElementById('navbarNavDropdown')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
 
 window.addEventListener('resize', () => {
-  if(window.innerWidth>=768) {
+  if(window.innerWidth<=768) {
     navLinks.forEach((l) => {
       l.addEventListener('click', () => { bsCollapse.toggle() })
   })
